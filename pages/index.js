0d336b26be94b7966103e288964c847e2bcd0de0
Dennys Matos <47328532+dennyscaetano@users.noreply.github.com>
2025-12-01 22:16:29 +0000

    fix: modifying one more the title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Título Alterado</h1>
+  return <h1>A determinação é o elemento que transforma intenção em conquista. O hábito de estudar, mesmo nos dias em que o cansaço pesa, molda a disciplina necessária para alcançar objetivos maiores. Um pouco de esforço diário cria um acúmulo de conhecimento que ninguém pode tirar de você.</h1>
 }
 
 export default Home
